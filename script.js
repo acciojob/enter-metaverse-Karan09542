e923a495f45cd6a0b4eff067e339d317efc99222
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-09-05 20:01:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const enterButton = document.getElementById("enterBtn");
+enterButton.onclick = function(){
+	const p = document.getElementById("status");
+	if(p){
+		p.remove();
+	}
+	const h1 = document.createElement("h1")
+	h1.textContent = "Entered Metaverse";
+	document.body.prepend(h1)
+	
+}
